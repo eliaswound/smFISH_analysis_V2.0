e41b769f89f1f7ba509cbb9cee56ba74b688b800
eliaswound <eliaswound@gmail.com>
2023-03-04 23:14:10 -0600

    decluster
    Added declustering feature

---
+++ 3.5_Cluster_detection/cluster_detection.py
@@ -0,0 +1,66 @@
+def cluster_decomposition(imarray,spots,voxel_size,spot_size,greeks,filepath = "."):
+    """
+    Decomposition of clusters
+    :param imarray: Image array
+    :param spots: spots detected
+    :param resolution: resolution
+    :param spot_size: spot size
+    :param greeks: greeks alpaha beta gamma
+    :return:
+    """
+    import os
+    import bigfish.detection
+    import bigfish.plot as plot
+    import bigfish.stack as stack
+    os.chdir(filepath)
+    alpha = greeks[0]
+    beta = greeks[1]
+    gamma = greeks[2]
+    spots_post_decomposition, dense_regions, reference_spot = bigfish.detection.decompose_dense(
+    image=imarray,
+    spots=spots,
+    voxel_size= voxel_size,
+    spot_radius= spot_size,
+    alpha=alpha,  # alpha impacts the number of spots per candidate region
+    beta=beta,  # beta impacts the number of candidate regions to decompose
+    gamma=gamma)  # gamma the filtering step to denoise the image
+    with open("spots_info_Decomposition.txt","w") as file :
+        file.write("detected spots before decomposition")
+        file.write("\r shape: {0}".format(spots.shape))
+        file.write("\r dtype: {0}".format(spots.dtype))
+        file.write("\n")
+        file.write("detected spots after decomposition")
+        file.write("\r shape: {0}".format(spots_post_decomposition.shape))
+        file.write("\r dtype: {0}".format(spots_post_decomposition.dtype))
+    return spots_post_decomposition, dense_regions, reference_spot
+def declustering(spot, voxel_size, deculstering_parameters,filepath = "."):
+    """
+    Declustering code
+    :param spots_post_decompositions: spots
+    :param resolution: resolution
+    :param deculstering_parameters:declustering parameters
+    imarray: image array
+    :return: spts and clusters
+    """
+    radius = deculstering_parameters[0]
+    nb_min_spots = deculstering_parameters[1]
+    import bigfish.detection
+    import os
+    import numpy as np
+    os.chdir(filepath)
+    spots_post_clustering, clusters = bigfish.detection.detect_clusters(
+        spots=spot,
+        voxel_size=voxel_size,
+        radius=radius,
+        nb_min_spots=nb_min_spots)
+    np.save('spot_post_cluster.npy', spots_post_clustering)
+    np.save('clusters.npy', clusters)
+    with open("Cluster_info.txt", "w") as file:
+        file.write("detected spots after clustering")
+        file.write("\r shape: {0}".format(spots_post_clustering.shape))
+        file.write("\r dtype: {0}".format(spots_post_clustering.dtype))
+        file.write("\n")
+        file.write("detected clusters")
+        file.write("\r shape: {0}".format(clusters.shape))
+        file.write("\r dtype: {0}".format(clusters.dtype))
+    return spots_post_clustering, clusters
---
+++ 3.5_Cluster_detection/cluster_plot.py
@@ -0,0 +1,154 @@
+def cluster_plot(image_shape, filepath):
+    """
+    :param np.shape of a image
+    :param spot: spot need to be plotted
+    :return: plotted image
+    """
+    import os
+    import numpy as np
+    os.chdir(filepath)
+    spot = np.load("clusters.npy")
+    empty_image = np.zeros(image_shape, dtype=np.uint8)
+    z_lim = empty_image.shape[0]-1
+    x_lim = empty_image.shape[1]-1
+    y_lim = empty_image.shape[2]-1
+    for i in range(spot.shape[0]):
+        spot_pos = np.ndarray.tolist(spot[i])
+        z = spot_pos[0]
+        x = spot_pos[1]
+        y = spot_pos[2]
+
+        empty_image[z, x, y] = 255
+        empty_image[z, x + 2, y] = 255
+        empty_image[z, x + 1, y + 1] = 255
+        empty_image[z, x + 1, y] = 255
+        empty_image[z, x + 1, y - 1] = 255
+        empty_image[z, x, y + 1] = 255
+        empty_image[z, x, y + 2] = 255
+        empty_image[z, x, y - 1] = 255
+        empty_image[z, x, y - 2] = 255
+        empty_image[z, x - 2, y] = 255
+        empty_image[z, x - 1, y + 1] = 255
+        empty_image[z, x - 1, y] = 255
+        empty_image[z, x - 1, y - 1] = 255
+        empty_image[z, x + 3, y] = 255
+        empty_image[z, x - 3, y] = 255
+        empty_image[z, x + 2, y + 1] = 255
+        empty_image[z, x + 2, y - 1] = 255
+        empty_image[z, x + 1, y + 2] = 255
+        empty_image[z, x + 1, y - 2] = 255
+        empty_image[z, x - 2, y - 1] = 255
+        empty_image[z, x - 2, y + 1] = 255
+        empty_image[z, x - 1, y - 2] = 255
+        empty_image[z, x - 1, y + 2] = 255
+        empty_image[z, x, y + 3] = 255
+        empty_image[z, x, y - 3] = 255
+        empty_image[z, x + 3, y] = 255
+        empty_image[z, x - 3, y] = 255
+        empty_image[z, x + 2, y + 1] = 255
+        empty_image[z, x + 2, y - 1] = 255
+        empty_image[z, x + 1, y + 2] = 255
+        empty_image[z, x + 1, y - 2] = 255
+        empty_image[z, x - 2, y - 1] = 255
+        empty_image[z, x - 2, y + 1] = 255
+        empty_image[z, x - 1, y - 2] = 255
+        empty_image[z, x - 1, y + 2] = 255
+        empty_image[z, x, y + 3] = 255
+        empty_image[z, x, y - 3] = 255
+        empty_image[z, x + 2, y - 2] = 255
+        empty_image[z, x - 2, y - 2] = 255
+        empty_image[z, x + 2, y + 2] = 255
+        empty_image[z, x - 2, y + 2] = 255
+    return empty_image
+def postcluster_detection_plot(image_shape, filepath):
+    """
+    :param np.shape of a image
+    :param spot: spot need to be plotted
+    :return: plotted image
+    """
+    import os
+    import numpy as np
+    os.chdir(filepath)
+    spot = np.load("spot_post_cluster.npy")
+    empty_image = np.zeros(image_shape, dtype=np.uint8)
+    z_lim = empty_image.shape[0]-1
+    x_lim = empty_image.shape[1]-1
+    y_lim = empty_image.shape[2]-1
+    for i in range(spot.shape[0]):
+        spot_pos = np.ndarray.tolist(spot[i])
+        z = spot_pos[0]
+        x = spot_pos[1]
+        y = spot_pos[2]
+        if x + 3 > x_lim or y + 3 > y_lim:
+            np.delete(spot, spot[i])
+        elif x - 3 > x_lim or y - 3 > y_lim:
+            np.delete(spot, spot[i])
+        elif z == 0 or z == z_lim:
+            np.delete(spot, spot[i])
+        else:
+            empty_image[z, x, y] = 255
+            empty_image[z, x + 2, y] = 255
+            empty_image[z, x + 1, y + 1] = 255
+            empty_image[z, x + 1, y] = 255
+            empty_image[z, x + 1, y - 1] = 255
+            empty_image[z, x, y + 1] = 255
+            empty_image[z, x, y + 2] = 255
+            empty_image[z, x, y - 1] = 255
+            empty_image[z, x, y - 2] = 255
+            empty_image[z, x - 2, y] = 255
+            empty_image[z, x - 1, y + 1] = 255
+            empty_image[z, x - 1, y] = 255
+            empty_image[z, x - 1, y - 1] = 255
+
+    return empty_image
+def postcluster_shadow_plot(image_shape, filepath):
+    """
+    :param image_shape: image shape
+    :param filepath: current file path
+    :return: shadow image
+    """
+    import numpy as np
+    import os
+    os.chdir(filepath)
+    spot = np.load("spot_post_cluster.npy")
+    empty_image2 = np.zeros(image_shape, dtype=np.uint8)
+    z_lim = empty_image2.shape[0] - 1
+    x_lim = empty_image2.shape[1] - 1
+    y_lim = empty_image2.shape[2] - 1
+    for i in range(spot.shape[0]):
+        spot_pos = np.ndarray.tolist(spot[i])
+        z = spot_pos[0]
+        x = spot_pos[1]
+        y = spot_pos[2]
+        if x + 3 > x_lim or y + 3 > y_lim:
+            np.delete(spot, spot[i])
+        elif x - 3 > x_lim or y - 3 > y_lim:
+            np.delete(spot, spot[i])
+        elif z == 1 or z == z_lim:
+            np.delete(spot, spot[i])
+        else:
+            empty_image2[z + 1, x + 3, y] = 255
+            empty_image2[z + 1, x - 3, y] = 255
+            empty_image2[z + 1, x + 2, y + 1] = 255
+            empty_image2[z + 1, x + 2, y - 1] = 255
+            empty_image2[z + 1, x + 1, y + 2] = 255
+            empty_image2[z + 1, x + 1, y - 2] = 255
+            empty_image2[z + 1, x - 2, y - 1] = 255
+            empty_image2[z + 1, x - 2, y + 1] = 255
+            empty_image2[z + 1, x - 1, y - 2] = 255
+            empty_image2[z + 1, x - 1, y + 2] = 255
+            empty_image2[z + 1, x, y + 3] = 255
+            empty_image2[z + 1, x, y - 3] = 255
+            empty_image2[z - 1, x + 3, y] = 255
+            empty_image2[z - 1, x - 3, y] = 255
+            empty_image2[z - 1, x + 2, y + 1] = 255
+            empty_image2[z - 1, x + 2, y - 1] = 255
+            empty_image2[z - 1, x + 1, y + 2] = 255
+            empty_image2[z - 1, x + 1, y - 2] = 255
+            empty_image2[z - 1, x - 2, y - 1] = 255
+            empty_image2[z - 1, x - 2, y + 1] = 255
+            empty_image2[z - 1, x - 1, y - 2] = 255
+            empty_image2[z - 1, x - 1, y + 2] = 255
+            empty_image2[z - 1, x, y + 3] = 255
+            empty_image2[z - 1, x, y - 3] = 255
+    return empty_image2
