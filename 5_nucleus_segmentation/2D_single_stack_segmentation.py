34ef4c18d10cd7266acbee8409f9a17ab4d62c47
eliaswound <eliaswound@gmail.com>
2024-03-22 01:41:55 -0500

    Update 2D_single_stack_segmentation.py

--- 5_nucleus_segmentation/2D_single_stack_segmentation.py
+++ 5_nucleus_segmentation/2D_single_stack_segmentation.py
@@ -1,5 +1,6 @@
 from __future__ import print_function, unicode_literals, absolute_import, division
 import sys
+import os
 import numpy as np
 import matplotlib
 matplotlib.rcParams["image.interpolation"] = 'none'
@@ -13,5 +14,43 @@ from csbdeep.io import save_tiff_imagej_compatible
 from stardist import random_label_cmap, _draw_polygons, export_imagej_rois
 from stardist.models import StarDist2D
 
+# Set up random seed for creating randomed colomaps
 np.random.seed(6)
 lbl_cmap = random_label_cmap()
+# Set dirctory, this will be the directory you need to run.
+os.chdir('/Users/eliasguan/Desktop/EG_Nucleus_Segementation_test/022523_smFISH_analysis_v2.0_development/Weekly_Reports/2024Spring/032424/DataSets/Test1')
+# glob file names.
+X = sorted(glob('*.tif'))
+X = list(map(imread,X))
+n_channel = 1 if X[0].ndim == 2 else X[0].shape[-1]
+axis_norm = (0,1)   # normalize channels independently
+# axis_norm = (0,1,2) # normalize channels jointly
+if n_channel > 1:
+    print("Normalizing image channels %s." % ('jointly' if axis_norm is None or 2 in axis_norm else 'independently'))
+if False:
+    fig, ax = plt.subplots(7,8, figsize=(16,16))
+    for i,(a,x) in enumerate(zip(ax.flat, X)):
+        a.imshow(x if x.ndim==2 else x[...,0], cmap='gray')
+        a.set_title(i)
+    [a.axis('off') for a in ax.flat]
+    plt.tight_layout()
+None;
+demo_model = True
+# Set up demo model
+if demo_model:
+    print (
+        "NOTE: This is loading a previously trained demo model!\n"
+        "      Please set the variable 'demo_model = False' to load your own trained model.",
+        file=sys.stderr, flush=True
+    )
+    model = StarDist2D.from_pretrained('2D_demo')
+else:
+    model = StarDist2D(None, name='stardist', basedir='models')
+None;
+# Run predictions
+img = normalize(X[0], 1,99.8, axis=axis_norm)
+labels, details = model.predict_instances(img)
+plt.figure(figsize=(8,8))
+plt.imshow(img if img.ndim==2 else img[...,0], clim=(0,1), cmap='gray')
+plt.imshow(labels, cmap=lbl_cmap, alpha=0.5)
+plt.axis('off');
